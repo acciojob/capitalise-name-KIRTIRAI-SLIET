4e6f00b21d9d72c84db9a7495849959226994c35
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-01-31 14:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function capsLetter(str) {
+	return str.toUpperCase()
+}
+
+const myElement = document.getElementById("fname").innerText;
+
+capsLetter(myElement);
